8d46d29e96b78ab2498f7ccb1b6f29c99986e34a
Heming <qiaohemingfcb@gmail.com>
2021-02-13 09:42:39 +0800

    add game

---
+++ game/js/game.js
@@ -0,0 +1,450 @@
+/**
+ * 类Level存储关卡对象
+ */
+class Level {
+  constructor(plan) {
+    let rows = plan.trim().split("\n").map(line => [...line]);
+    this.height = rows.length;
+    this.width = rows[0].length;
+    this.startActors = [];
+
+    this.rows = rows.map((row, y) => {
+      return row.map((ch, x) => {
+        let type = levelChars[ch];
+        if (typeof type == "string") return type;
+        this.startActors.push(
+          type.create(new Vec(x, y), ch)
+        );
+        return "empty";
+      });
+    });
+    // console.log(this);
+    // console.log(this.rows);
+  }
+}
+
+Level.prototype.touches = function (pos, size, type) {
+  let xStart = Math.floor(pos.x);
+  let xEnd = Math.ceil(pos.x + size.x);
+  let yStart = Math.floor(pos.y);
+  let yEnd = Math.ceil(pos.y + size.y);
+
+  for (let y = yStart; y < yEnd; y++) {
+    for (let x = xStart; x < xEnd; x++) {
+      let isOutside = x < 0
+        || x >= this.width
+        || y < 0
+        || y >= this.height;
+      let here = isOutside ? "wall" : this.rows[y][x];
+      if (here == type) return true;
+    }
+  }
+  return false;
+}
+
+
+/**
+ * State类用来跟踪正在运行的游戏的状态。
+ *
+ * 这是一个持久的数据结构，更新游戏状态会创建一个新状态并使旧状态保持不变。
+ */
+class State {
+  constructor(level, actors, status) {
+    this.level = level;
+    this.actors = actors;
+    this.status = status;
+  }
+
+  static start(level) {
+    return new State(level, level.startActors, "playing");
+  }
+
+  get player() {
+    return this.actors.find(a => a.type == "player");
+  }
+}
+
+State.prototype.update = function (time, keys) {
+  let actors = this.actors.map(actor => actor.update(time, this, keys));
+  let newState = new State(this.level, actors, this.status);
+
+  if (newState.status != "playing") return newState;
+
+  let player = newState.player;
+  if (this.level.touches(player.pos, player.size, "lava")) {
+    return new State(this.level, actors, "lost");
+  }
+
+  for (let actor of actors) {
+    if (actor != player && overlap(actor, player)) {
+      newState = actor.collide(newState);
+    }
+  }
+  return newState;
+}
+
+function overlap(actor1, actor2) {
+  return actor1.pos.x + actor1.size.x > actor2.pos.x
+    && actor1.pos.x < actor2.pos.x + actor2.size.x
+    && actor1.pos.y + actor1.size.y > actor2.pos.y
+    && actor1.pos.y < actor2.pos.y + actor2.size.y;
+}
+
+
+/**
+ * Vec类用于存储二维的值，如演员的位置坐标和大小。
+ */
+class Vec {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  plus(other) {
+    return new Vec(this.x + other.x, this.y + other.y);
+  }
+
+  /**
+   * 根据给定因子缩放矢量
+   * @param factor
+   * @return {Vec}
+   */
+  times(factor) {
+    return new Vec(this.x * factor, this.y * factor);
+  }
+}
+
+
+class Player {
+  constructor(pos, speed) {
+    this.pos = pos;
+    this.speed = speed;
+  }
+
+  get type() {
+    return "player";
+  }
+
+  static create(pos) {
+    return new Player(
+      pos.plus(new Vec(0, -0.5)),
+      new Vec(0, 0)
+    );
+  }
+}
+
+Player.prototype.size = new Vec(0.8, 1.5);
+
+const playerXSpeed = 7;
+const gravity = 30;
+const jumpSpeed = 17;
+
+Player.prototype.update = function (time, state, keys) {
+  let xSpeed = 0;
+  if (keys.ArrowLeft) xSpeed -= playerXSpeed;
+  if (keys.ArrowRight) xSpeed += playerXSpeed;
+  let pos = this.pos;
+  let movedX = pos.plus(new Vec(xSpeed * time, 0));
+  if (!state.level.touches(movedX, this.size, "wall")) {
+    pos = movedX;
+  }
+
+  let ySpeed = this.speed.y + time * gravity;
+  let movedY = pos.plus(new Vec(0, ySpeed * time));
+  if (!state.level.touches(movedY, this.size, "wall")) {
+    pos = movedY;
+  } else if (keys.ArrowUp && ySpeed > 0) {
+    ySpeed = -jumpSpeed;
+  } else {
+    ySpeed = 0;
+  }
+
+  return new Player(pos, new Vec(xSpeed, ySpeed));
+};
+
+
+function trackKeys(keys) {
+  let down = Object.create(null);
+  function track(event) {
+    if (keys.includes(event.key)) {
+      down[event.key] = event.type == "keydown";
+      event.preventDefault();
+    }
+  }
+
+  window.addEventListener("keydown", track);
+  window.addEventListener("keyup", track);
+  return down;
+}
+
+const arrowKeys = trackKeys(["ArrowLeft", "ArrowRight", "ArrowUp"]);
+
+
+class Lava {
+  constructor(pos, speed, reset) {
+    this.pos = pos;
+    this.speed = speed;
+    this.reset = reset;
+  }
+
+  get type() {
+    return "lava";
+  }
+
+  static create(pos, ch) {
+    if (ch == "=") {
+      return new Lava(pos, new Vec(2, 0));
+    } else if (ch == "|") {
+      return new Lava(pos, new Vec(0, 2));
+    } else if (ch == "v") {
+      return new Lava(pos, new Vec(0, 3), pos);
+    }
+  }
+}
+
+Lava.prototype.size = new Vec(1, 1);
+
+Lava.prototype.collide = function (state) {
+  return new State(state.level, state.actors, "lost");
+};
+
+Lava.prototype.update = function (time, state) {
+  let newPos = this.pos.plus(this.speed.times(time));
+  if (!state.level.touches(newPos, this.size, "wall")) {
+    return new Lava(newPos, this.speed, this.reset);
+  } else if (this.reset) {
+    return new Lava(this.reset, this.speed, this.reset);
+  } else {
+    return new Lava(this.pos, this.speed.times(-1));
+  }
+};
+
+
+class Coin {
+  constructor(pos, basePos, wobble) {
+    this.pos = pos;
+    this.basePos = basePos;
+    this.wobble = wobble;
+  }
+
+  get type() {
+    return "coin";
+  }
+
+  static create(pos) {
+    let basePos = pos.plus(new Vec(0.2, 0.1));
+    return new Coin(
+      basePos,
+      basePos,
+      Math.random() * Math.PI * 2
+    );
+  }
+}
+
+Coin.prototype.size = new Vec(0.6, 0.6);
+
+Coin.prototype.collide = function (state) {
+  let filtered = state.actors.filter(a => a != this);
+  let status = state.status;
+  if (!filtered.some(a => a.type == "coin")) status = "won";
+  return new State(state.level, filtered, status);
+};
+
+const wobbleSpeed = 8, wobbleDist = 0.07;
+
+Coin.prototype.update = function (time) {
+  let wobble = this.wobble + time * wobbleSpeed;
+  let wobblePos = Math.sin(wobble) * wobbleDist;
+  return new Coin(
+    this.basePos.plus(new Vec(0, wobblePos)),
+    this.basePos,
+    wobble
+  );
+};
+
+
+/**
+ * 存储字符和其对应的类型。
+ *
+ * 将背景字符映射到字符串，将演员字符映射到类。
+ */
+const levelChars = {
+  ".": "empty",   // 点代表空白（空气）
+  "#": "wall",    // #代表墙壁
+  "+": "lava",    // +代表熔岩
+  "@": Player,    // @代表游戏玩家
+  "o": Coin,      // o代表一个硬币
+  "=": Lava,      // =代表一块水平来回移动的熔岩块
+  "|": Lava,      // |代表一块垂直来回移动的熔岩块
+  "v": Lava,      // v代表一块只能向下移动的熔岩块
+};
+
+let simpleLevelPlan = `
+......................
+..#................#..
+..#..............=.#..
+..#.........o.o....#..
+..#.@......#####...#..
+..#####............#..
+......#++++++++++++#..
+......##############..
+......................`;
+
+// let simpleLevel = new Level(simpleLevelPlan);
+// console.log(`${simpleLevel.width} by ${simpleLevel.height}`);
+
+
+/* ====================================================================================== */
+
+function elt(name, attrs, ...children) {
+  let dom = document.createElement(name);
+  for (let attr of Object.keys(attrs)) {
+    dom.setAttribute(attr, attrs[attr]);
+  }
+  for (let child of children) {
+    dom.appendChild(child);
+  }
+  return dom;
+}
+
+class DOMDisplay {
+  constructor(parent, level) {
+    this.dom = elt("div", {class: "game"}, drawGrid(level));
+    this.actorLayer = null;
+    parent.appendChild(this.dom);
+  }
+
+  // 从DOM中删除元素
+  clear() {
+    this.dom.remove();
+  }
+}
+
+DOMDisplay.prototype.syncState = function (state) {
+  // 首先从DOM中删除旧的演员元素
+  if (this.actorLayer) this.actorLayer.remove();
+  this.actorLayer = drawActors(state.actors);
+  this.dom.appendChild(this.actorLayer);
+  this.dom.className = `game ${state.status}`;
+  this.scrollPlayerIntoView(state);
+};
+
+DOMDisplay.prototype.scrollPlayerIntoView = function (state) {
+  let width = this.dom.clientWidth;
+  let height = this.dom.clientHeight;
+  let margin = width / 3;
+
+  // 视口范围
+  let left = this.dom.scrollLeft, right = left + width;
+  let top = this.dom.scrollTop, bottom = top + height;
+
+  let player = state.player;
+  let center = player.pos
+    .plus(player.size.times(0.5))
+    .times(scale);
+
+  if (center.x < left + margin) {
+    this.dom.scrollLeft = center.x - margin;
+  } else if (center.x > right - margin) {
+    this.dom.scrollLeft = center.x + margin - width;
+  }
+  if (center.y < top + margin) {
+    this.dom.scrollTop = center.y - margin;
+  } else if (center.y > bottom - margin) {
+    this.dom.scrollTop = center.y + margin - height;
+  }
+}
+
+
+const scale = 20;
+
+function drawGrid(level) {
+  return elt(
+    "table",
+    {
+      class: "background",
+      style: `width: ${level.width * scale}px`,
+    },
+    ...level.rows.map(row => {
+      return elt(
+        "tr",
+        {style: `height: ${scale}px`},
+        ...row.map(type => elt("td", {class: type}))
+      );
+    })
+  );
+}
+
+
+function drawActors(actors) {
+  return elt(
+    "div",
+    {},
+    ...actors.map(actor => {
+      let rect = elt("div", {class: `actor ${actor.type}`});
+      rect.style.cssText = `
+        width: ${actor.size.x * scale}px;
+        height: ${actor.size.y * scale}px;
+        left: ${actor.pos.x * scale}px;
+        top: ${actor.pos.y * scale}px;
+      `;
+      // rect.style.width = `${actor.size.x * scale}px`;
+      // rect.style.height = `${actor.size.y * scale}px`;
+      // rect.style.left = `${actor.pos.x * scale}px`;
+      // rect.style.top = `${actor.pos.y * scale}px`;
+      return rect;
+    })
+  );
+}
+
+
+/* ============================================================================================ */
+
+
+function runAnimation(frameFunc) {
+  let lastTime = null;
+  function frame(time) {
+    if (lastTime != null) {
+      // ms转化成s
+      let timeStep = Math.min(time - lastTime, 100) / 1000;
+      if (frameFunc(timeStep) === false) return;
+    }
+    lastTime = time;
+    requestAnimationFrame(frame);
+  }
+  requestAnimationFrame(frame);
+}
+
+
+function runLevel(level, Display) {
+  let display = new Display(document.body, level);
+  let state = State.start(level);
+  let ending = 1;
+
+  return new Promise(resolve => {
+    runAnimation(time => {
+      state = state.update(time, arrowKeys);
+      display.syncState(state);
+      if (state.status == "playing") {
+        return true;
+      } else if (ending > 0) {
+        ending -= time;
+        return true;
+      } else {
+        display.clear();
+        resolve(state.status);
+        return false;
+      }
+    });
+  });
+}
+
+
+async function runGame(plans, Display) {
+  for (let level = 0; level < plans.length; ) {
+    let status = await runLevel(new Level(plans[level]), Display);
+    if (status == "won") level++;
+  }
+  alert("You have won!");
+}
+
